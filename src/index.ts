af8c55b568da1c3ec0cf37a50d273a287667ebb9
Saurav3004 <130168711+Saurav3004@users.noreply.github.com>
2025-08-27 21:09:03 +0530

    created rooms

--- src/index.ts
+++ src/index.ts
@@ -2,17 +2,32 @@ import {WebSocket, WebSocketServer} from "ws";
 
 const wss = new WebSocketServer({port:8080});
 
-let userCount = 0;
-let allSockets: WebSocket[] = [];
+interface User {
+    socket:WebSocket,
+    room:string
+}
+
+let allSockets: User[] = [];
 wss.on("connection",function(socket){
     console.log("user connected");
     
-    userCount++;
-    allSockets.push(socket)
     
-    socket.on("message",(message) => {
-        allSockets.forEach(s => {
-        s.send(message.toString() + " sent from the server")
+    socket.on("message",(message ) => {
+        const parsedMessage = JSON.parse(message as unknown as string);
+        if(parsedMessage.type === "join"){
+            allSockets.push({
+                socket,
+                room: parsedMessage.payload.roomId
+            })
+        }
+
+        if(parsedMessage.type ==="chat"){
+            let currentUserRoom = null;
+            for(let i = 0; i<allSockets.length;i++){
+                if(allSockets[i].socket == socket){
+                    currentUserRoom = allSockets[i].room
+                }
+            }
+        }
     })
     })
-})
